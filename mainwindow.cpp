ef503f6716d4d5cf0e4e8b277d6ea9a7f539a96d
adrianavallazza <adriana.vallazza@gmx.at>
2019-10-16 17:19:08 +0200

    Added Control Layout and Slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,121 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->gridLayout->setAlignment(Qt::AlignCenter);
-
-    for(int i= 0; i<ui->gridLayout->columnCount(); i++) {
-            //ui->gridLayout->setColumnMinimumWidth(i, 10);
-    }
+    // ui->textEditDisplay->setAlignment(Qt::AlignCenter);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButtonRectangle_clicked()
+{
+  // Start rectangle movement
+}
+
+void MainWindow::on_pushButtonCircle_clicked()
+{
+  // Start circle movement
+}
+
+void MainWindow::on_pushButtonLine_clicked()
+{
+  // start line movement
+}
+
+void MainWindow::on_pushButtonTriangle_clicked()
+{
+  // start triangle movement
+}
+
+void MainWindow::on_pushButtonStar_clicked()
+{
+  // start star movement
+}
+
+
+// -------------------------------------------------------------------
+// Arrow Buttons
+
+void MainWindow::on_pushButtonUp_pressed()
+{
+  // Set power of left and right motor to equal percentage
+  // Extract power value from Slider value
+  ui->textEditDisplay->setText("Driving forward (Speed = "+QString::number(ui->horizontalSliderSpeed->value())+")");
+}
+
+void MainWindow::on_pushButtonUp_released()
+{
+  // Set power of left and right motor to 0% to stop
+  ui->textEditDisplay->setText("");
+}
+
+void MainWindow::on_pushButtonRight_pressed()
+{
+  // Rotate to the right
+  // Set power of left and right motor to percentage
+  // Extract power value from Slider value
+  ui->textEditDisplay->setText("Driving right (Speed = "+QString::number(ui->horizontalSliderSpeed->value())+")");
+}
+
+void MainWindow::on_pushButtonRight_released()
+{
+  // Set power of left and right motor to 0% to stop
+  ui->textEditDisplay->setText("");
+}
+
+void MainWindow::on_pushButtonDown_pressed()
+{
+  // Set power of left and right motor to negative percentage
+  // Extract power value from Slider value
+  ui->textEditDisplay->setText("Driving backwards (Speed = "+QString::number(ui->horizontalSliderSpeed->value())+")");
+}
+
+void MainWindow::on_pushButtonDown_released()
+{
+  // Set power of left and right motor to 0% to stop
+  ui->textEditDisplay->setText("");
+}
+
+void MainWindow::on_pushButtonLeft_pressed()
+{
+  // Rotate to the left
+  // Set power of left and right motor to percentage
+  // Extract power value from Slider value
+  ui->textEditDisplay->setText("Driving left (Speed = "+QString::number(ui->horizontalSliderSpeed->value())+")");
+}
+
+void MainWindow::on_pushButtonLeft_released()
+{
+  // Set power of left and right motor to 0% to stop
+  ui->textEditDisplay->setText("");
+}
+
+
+// ---------------------------------------------------------------------------
+
+void MainWindow::on_pushButtonLed_clicked()
+{
+  // Start Led Effect
+}
+
+
+
+void MainWindow::on_pushButtonSound_clicked()
+{
+  // Start Sound Effect
+}
+
+void MainWindow::on_horizontalSliderSpeed_sliderMoved(int position)
+{
+    ui->textEditDisplay->setText("Slider moved " + QString::number(position));
+      /*
+    ui->labelCurrSliderPos->setGeometry(ui->horizontalSliderSpeed->cursor().pos().x(),
+                             ui->horizontalSliderSpeed->cursor().pos().y(),
+                             20,
+                             20);
+                             */
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,41 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonRectangle_clicked();
+
+    void on_pushButtonCircle_clicked();
+
+    void on_pushButtonLine_clicked();
+
+    void on_pushButtonTriangle_clicked();
+
+    void on_pushButtonStar_clicked();
+
+    void on_pushButtonUp_pressed();
+
+    void on_pushButtonUp_released();
+
+    void on_pushButtonRight_pressed();
+
+    void on_pushButtonRight_released();
+
+    void on_pushButtonDown_pressed();
+
+    void on_pushButtonDown_released();
+
+    void on_pushButtonLeft_pressed();
+
+    void on_pushButtonLeft_released();
+
+    void on_pushButtonLed_clicked();
+
+    void on_pushButtonSound_clicked();
+
+    void on_horizontalSliderSpeed_sliderMoved(int position);
+
+
+
 private:
     Ui::MainWindow *ui;
 };
